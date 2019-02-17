f5a484c050bf94bf4cc38495f51b39c857862e4e
gurayci <guray.cintir@argela.com.tr>
2019-02-17 23:21:22 +0300

    springboot-scratch-scheduling app commit

--- scheduling/src/main/java/springboot/scratch/scheduling/SchedulingApplication.java
+++ scheduling/src/main/java/springboot/scratch/scheduling/SchedulingApplication.java
@@ -1,14 +1,21 @@
 package springboot.scratch.scheduling;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class SchedulingApplication {
+@EnableScheduling
+public class SchedulingApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulingApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+	}
 
+}
