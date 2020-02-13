9136a3d216f7224ef2ca8f5121239e0c7a822a94
gcintir <guraycintir@gmail.com>
2020-02-13 23:42:06 +0300

    initial commit

---
+++ rest-api/src/main/java/springboot/scratch/restapi/UserDto.java
@@ -0,0 +1,35 @@
+package springboot.scratch.restapi;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class UserDto {
+
+
+    private String userId;
+
+    @NotNull(message = "First name can not be null")
+    private String firstName;
+
+    @NotNull(message = "Last name can not be null")
+    private String lastName;
+
+    @NotNull(message = "Email can not be null")
+    @Email(message = "Invalid email")
+    private String email;
+
+    @NotNull(message = "Password can not be null")
+    @Size(min = 5, max = 10, message = "Password length must be between 5 and 10 characters")
+    private String password;
+
+}
